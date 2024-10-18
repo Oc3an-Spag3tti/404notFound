f1c527e7f8c64874e4018919d288232bf6647817
Lbutynski <lucas.butynski@supdevinci-edu.fr>
2024-10-18 16:13:49 +0200

    feat: CRUD for categories

--- backend/src/routes/categories.ts
+++ backend/src/routes/categories.ts
@@ -10,4 +10,13 @@ CategoriesRouteur.post("/", async (req: Request, res: Response) => {
   console.log(req.body);
   res.send(await Categories.insertMany(req.body));
 });
+CategoriesRouteur.get("/:id", async (req: Request, res: Response) => {
+  res.send(await Categories.find({ _id: req.params.id }));
+});
+CategoriesRouteur.put("/:id", async (req: Request, res: Response) => {
+  res.send(await Categories.updateOne({ _id: req.params.id }, req.body));
+});
+CategoriesRouteur.delete("/:id", async (req: Request, res: Response) => {
+  res.send(await Categories.deleteOne({ _id: req.params.id }));
+});
 export default CategoriesRouteur;
