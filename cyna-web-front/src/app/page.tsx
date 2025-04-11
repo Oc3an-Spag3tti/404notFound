d5d576cd5470818dc17c0690658eeb6dcdabe75b
BillyCodeDev <ang.billy@yahoo.com>
2025-04-11 17:25:59 +0100

    Update src arbo : pages to app

--- cyna-web-front/src/app/page.tsx
+++ cyna-web-front/src/app/page.tsx
@@ -1,16 +1,12 @@
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
 import Carousel from "@/components/Carousel";
 // import CategoriesSection from "@/components/CategoriesSection";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
       <main className="flex-grow flex items-center justify-center">
         <Carousel />
       </main>
-      <Footer />
     </div>
   );
 }
--- cyna-web-front/src/app/products/[idProduct]/page.tsx
+++ cyna-web-front/src/app/products/[idProduct]/page.tsx
@@ -1,5 +1,6 @@
+"use client";
+
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 
 interface Product {
   id: string;
@@ -8,23 +9,20 @@ interface Product {
   price: number;
 }
 
-const ProductPage = () => {
-  const router = useRouter();
-  const { idProduct } = router.query;
-
+const ProductPage = ({ params }: { params: { idProduct: string } }) => {
   const [product, setProduct] = useState<Product | null>(null);
 
   // recup infos product
   useEffect(() => {
-    if (idProduct) {
-      fetch(`http://localhost:3001/products/${idProduct}`)
+    if (params.idProduct) {
+      fetch(`http://localhost:3001/products/${params.idProduct}`)
         .then((res) => res.json())
         .then((data) => setProduct(data))
         .catch((err) =>
           console.error("Erreur lors de la récupération du produit :", err)
         );
     }
-  }, [idProduct]);
+  }, [params.idProduct]);
 
   return (
     <div>
@@ -36,18 +34,3 @@ const ProductPage = () => {
 };
 
 export default ProductPage;
-//   `http://localhost:3001/products/${params.productId}`,
-//   {
-//     cache: "no-store",
-//   }
-// );
-
-// const product = await res.json();
-
-// return (
-//   <div className="p-8">
-//     <h1 className="text-3xl font-bold">{product.name}</h1>
-//     <p className="text-gray-600">{product.description}</p>
-//     <p className="text-xl mt-4">Prix : {product.price} €</p>
-//   </div>
-// );
