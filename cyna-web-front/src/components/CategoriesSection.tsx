0ab2442c4f342810e12e2ca1debd0ff69af2065b
BillyCodeDev <ang.billy@yahoo.com>
2025-04-13 13:06:15 +0100

    Add grid categories

--- cyna-web-front/src/components/CategoriesSection.tsx
+++ cyna-web-front/src/components/CategoriesSection.tsx
@@ -23,23 +23,18 @@ const CategoriesSection = () => {
   }, []);
 
   return (
-    <>
-      <div>
-        <ul></ul>
-      </div>
-      <div className="items-center justify-center">
-        {categories.map((category) => (
-          <div
-            key={category._id}
-            className="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 max-w-full"
-          >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {category.name}
-            </h5>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-wrap m-8 justify-center lg:gap-10 lg:w-full lg:max-w-5xl">
+      {categories.map((category) => (
+        <div
+          key={category._id}
+          className="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 max-w-full"
+        >
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {category.name}
+          </h5>
+        </div>
+      ))}
+    </div>
   );
 };
 export default CategoriesSection;
